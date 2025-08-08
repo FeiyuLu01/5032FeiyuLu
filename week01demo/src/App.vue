16256b82049a51940eb5bfb8384793f773a86c43
Feiyu-Lu <fluu0019@student.monash.edu>
2025-08-08 19:38:07 +1000

    week1 demo

---
+++ week01demo/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/FeiyuLu.vue';
+
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style scoped>
+
+</style>
---
+++ week01demo/src/components/FeiyuLu.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="greetings">
+    I am Feiyu Lu, my student id is 34482512
+  </h1>
+</template>
+
+<style scoped>
+
+.greetings {
+  text-align: center;
+  color: aqua;
+}
+
+</style>
